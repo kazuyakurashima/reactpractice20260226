d8e1022bd5c80a9feb329f3f55e004caada854e6
kazuyakurashima <gs.kaz224@gmail.com>
2026-03-08 12:12:00 +0900

    ああ

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import CardExample from "./CardExample"
+import EventExample from "./EventExample"
 
 const App = () => {
 
   return (
     <div className="min-h-screen bg-gray-200">
       <div className="bg-gray-300 p-4">
+        <EventExample />
         <CardExample />
       </div>
     </div>

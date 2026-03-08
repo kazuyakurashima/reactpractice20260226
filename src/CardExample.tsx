4984daa5da177a532487ff93c8e4d4706876d6c5
kazuyakurashima <gs.kaz224@gmail.com>
2026-03-08 09:34:15 +0900

    ってみたメイニ師 git commit -m やってみた、ホメイニ師”

--- src/CardExample.tsx
+++ src/CardExample.tsx
@@ -1,10 +1,39 @@
 import Card from "./Card";
 
+    const handleClick = (e) => {
+        alert('もう！押さないでっていったのに！激おこ！')
+    }
+
+    const showMessage = (message) => {
+        alert(message)
+    }
+
+    const showTime = () => {
+        const now = new Date()
+        alert(`現在の時刻： ${now.toLocaleTimeString()}`)
+    }
+
 const CardExample = () => {
     return (
         <div className="p-8">
             <h1 className="text-2xl font-bold mb-6">Childrenの活用例</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+                <button
+                    onClick={() => showMessage('ボタンって言ったでしょ。牡丹')}
+                    className="block bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600"
+                >
+                    これがぼたんじゃ
+                </button>
+
+                <button
+                    onClick={showTime}
+                    className="block bg-green-500 text-white px-6 py-3 rounded-lg hover:bg-green"
+                >
+                    ショウタイム
+                </button>
+
+            
                 <Card title="ホメイニ師">
                     <p className="text-gray-700">
                         イランの最高指導者
@@ -25,8 +54,10 @@ const CardExample = () => {
                     <p className="text-gray-700 mb-4">
                         このカードにはボタンが含まれています
                     </p>
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                        アクション
+                    <button
+                        onClick={handleClick}
+                        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                            絶対に押さないで！
                     </button>
                 </Card>
 
